e987a6567715e207f29b8be0d11de198a395c44f
Brad Pizzimenti and Lindsey Raffa <lindsey.raffa@gmail.com>
2016-01-27 14:03:07 -0800

    add gameWon prototype of TicTacBoard to test for winning combinations

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,70 @@ function Player(playerName,mark,isTurn) {
 }
 
 
-function TicTacBoard(a1,a2,a3) {
+function TicTacBoard(a1,a2,a3,b1,b2,b3,c1,c2,c3) {
   this.a1 = a1;
   this.a2 = a2;
   this.a3 = a3;
+  this.b1 = b1;
+  this.b2 = b2;
+  this.b3 = b3;
+  this.c1 = c1;
+  this.c2 = c2;
+  this.c3 = c3;
+}
+
+TicTacBoard.prototype.boardArray = function() {
+return [this.a1,this.a2,this.a3,this.b1,this.b2,this.b3,this.c1,this.c2,this.c3];
+}
+
+// var myBoard = new TicTacBoard(1,1,1,0,0,0,0,0,0);
+
+TicTacBoard.prototype.gameWon = function() {
+
+  if (this.a1 + this.a2 + this.a3 === 3) {
+    return "Player 1 Wins in the left column";
+  }
+
+  if (this.b1 + this.b2 + this.b3 === 3) {
+    return "Player 1 Wins in the middle column";
+  }
+
+  if (this.c1 + this.c2 + this.c3 === 3) {
+    return "Player 1 Wins in the right column";
+  }
+
+  if (this.a1 + this.b1 + this.c1 === 3) {
+    return "Player 1 Wins in the bottom row";
+  }
+
+  if (this.a2 + this.b2 + this.c2 === 3) {
+    return "Player 1 Wins in the middle row";
+  }
+
+  if (this.a3 + this.b3 + this.c3 === 3) {
+    return "Player 1 Wins in the top row";
+  }
+
+  if (this.a1 + this.b2 + this.c3 === 3) {
+    return "Player 1 Wins upward diagonal";
+  }
+
+  if (this.a3 + this.b2 + this.c1 === 3) {
+    return "Player 1 Wins downward diagonal";
+  }
+
 
 }
 
-//
-// TicTacBoard.prototype.a1x = function() {
-//   if (this.a1 === 0) {
-//     this.a1 = 1
-//   } else {
-//     return "square not empty"
-//   }
-// }
+
+
+TicTacBoard.prototype.a1x = function() {
+  if (this.a1 === 0) {
+    this.a1 = 1
+  } else {
+    return "square not empty"
+  }
+}
 //
 // TicTacBoard.prototype.a1o = function() {
 //   this.a1 = -1
--- spec/specs.js
+++ spec/specs.js
@@ -10,7 +10,7 @@ describe('Player', function() {
 
 describe('TicTacBoard', function() {
   it("is true that Player 1 has won in the left column", function() {
-    var testBoard = new TicTacBoard(1,1,1);
+    var testBoard = new TicTacBoard(1,1,1,0,0,0,0,0,0);
     expect(testBoard.a1).to.equal(1);
     expect(testBoard.a2).to.equal(1);
     expect(testBoard.a3).to.equal(1);
@@ -22,6 +22,51 @@ describe('TicTacBoard', function() {
     expect(testBoard.a2).to.equal(-1);
     expect(testBoard.a3).to.equal(-1);
     expect(testBoard.a1 + testBoard.a2 + testBoard.a3).to.equal(-3);
-  })
-  
+
+  });
 });
+
+describe('Player 1 Wins', function() {
+  it("is true that Player 1 has won in left column", function() {
+    var testBoard = new TicTacBoard(1,1,1);
+    expect(testBoard.gameWon()).to.equal("Player 1 Wins in the left column");
+  });
+  it("is true that Player 1 has won in middle column", function() {
+    var testBoard = new TicTacBoard(0,0,0,1,1,1,0,0,0);
+    expect(testBoard.gameWon()).to.equal("Player 1 Wins in the middle column");
+  });
+  it("is true that Player 1 has won in right column", function() {
+    var testBoard = new TicTacBoard(0,0,0,0,0,0,1,1,1);
+    expect(testBoard.gameWon()).to.equal("Player 1 Wins in the right column");
+  });
+  it("is true that Player 1 has won the downward diagonal", function() {
+    var testBoard = new TicTacBoard(0,0,1,0,1,0,1,0,0);
+    expect(testBoard.gameWon()).to.equal("Player 1 Wins downward diagonal");
+  });
+});
+
+describe('Player 2 Wins', function() {
+  it("is true that Player 2 has won in left column", function() {
+    var testBoard = new TicTacBoard(-1,-1,-1);
+    expect(testBoard.gameWon()).to.equal("Player 2 Wins in the left column");
+  });
+  it("is true that Player 2 has won in middle column", function() {
+    var testBoard = new TicTacBoard(0,0,0,-1,-1,-1,0,0,0);
+    expect(testBoard.gameWon()).to.equal("Player 2 Wins in the middle column");
+  });
+  it("is true that Player 2 has won in right column", function() {
+    var testBoard = new TicTacBoard(0,0,0,0,0,0,-1,-1,-1);
+    expect(testBoard.gameWon()).to.equal("Player 2 Wins in the right column");
+  });
+  it("is true that Player 2 has won the downward diagonal", function() {
+    var testBoard = new TicTacBoard(0,0,-1,0,-1,0,-1,0,0);
+    expect(testBoard.gameWon()).to.equal("Player 2 Wins downward diagonal");
+  });
+});
+
+// describe('GameTied', function() {
+//   it("is true that there are still empty squares", function() {
+//     var testBoard = new TicTacBoard(1,1,-1,1,-1,-1,0,1,-1);
+//     expect(GameLogic).to.equal("game continues")
+//   });
+// });
